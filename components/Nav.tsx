2b7df664db3e0f2adc73387fb53caf81ae2eb82a
Willake <willakelin@gmail.com>
2021-12-15 10:54:42 +0800

    navbar completed

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -4,6 +4,7 @@ import { Squash as Hamburger } from 'hamburger-react';
 import React, { useState } from 'react';
 import Link from 'next/link';
 import Menu from './Nav/Menu';
+import classNames from 'classnames';
 
 const Nav: React.FC = () => {
     const [isOpen, setOpen] = useState(false);
@@ -12,36 +13,36 @@ const Nav: React.FC = () => {
         setOpen(false);
     }
 
-    let menu = isOpen ? <Menu closeMenu={closeMenu}/> : <></>;
+    let menu = isOpen ? <Menu isOpen={isOpen} closeMenu={closeMenu}/> : <></>;
 
     return (
     <nav className={styles.nav}>
-        <div className={styles.logo}>
+        <div className={classNames(styles.logo, 'hoverable')}>
             <Link href="/">
                 <Image src='/images/logo.svg' alt="logo" layout="fill"/>
             </Link>
         </div>
         <div className={styles.title}>
-            <h1>Hui En Lin</h1>
+            <h1 className={classNames('xl', 'colorWhite')}>Hui En Lin</h1>
         </div>
         <div className={styles.menuButton}>
             <Hamburger toggled={isOpen} toggle={setOpen} />
         </div>
         <div className={styles.links}>
             <ul>
-                <li>
+                <li className={'hoverable'}>
                     <Link href="/">
-                        <a>Home</a>
+                        <a className={classNames('xl', 'colorWhite')}>Home</a>
                     </Link>
                 </li>
-                <li>
+                <li className={'hoverable'}>
                     <Link href="/about">
-                        <a>About</a>
+                        <a className={classNames('xl', 'colorWhite')}>About</a>
                     </Link>
                 </li>
-                <li>    
+                <li className={'hoverable'}>    
                     <Link href="/#portfolio">
-                        <a>Portfolio</a>
+                        <a className={classNames('xl', 'colorWhite')}>Portfolio</a>
                     </Link>
                 </li>
             </ul>
--- components/Nav/Menu.tsx
+++ components/Nav/Menu.tsx
@@ -1,31 +1,38 @@
 import styles from '../../styles/Nav.module.scss';
 import React from 'react';
 import Link from 'next/link';
+import classNames from 'classnames';
+import { FaTimes } from "react-icons/fa";
 
 type Props = {
+    isOpen: boolean,
     closeMenu: () => void
 }
 
-const Menu: React.FC<Props> = ({closeMenu}) => {
-
+const Menu: React.FC<Props> = ({isOpen, closeMenu}) => {
     return (
     <>
         <div onClick={closeMenu} className={styles.dim}></div>
         <div className={styles.menu}>
+            <div className={styles.menuHeader}>
+                <div onClick={closeMenu} className={classNames(styles.closeButton, 'colorWhite')}>
+                    <FaTimes size={46}/>
+                </div>
+            </div>
             <ul>
                 <li>
                     <Link href="/">
-                        <a>Home</a>
+                        <a className={classNames('xl', 'colorWhite')}>Home</a>
                     </Link>
                 </li>
                 <li>
                     <Link href="/about">
-                        <a>About</a>
+                        <a className={classNames('xl', 'colorWhite')}>About</a>
                     </Link>
                 </li>
                 <li>    
                     <Link href="/#portfolio">
-                        <a>Portfolio</a>
+                        <a className={classNames('xl', 'colorWhite')}>Portfolio</a>
                     </Link>
                 </li>
             </ul>
