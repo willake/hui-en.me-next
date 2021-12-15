428570276157ebaae0a6c74ca9a2accdf32d4f9a
Willake <willakelin@gmail.com>
2021-12-15 11:43:35 +0800

    update skill

--- components/About/Skill.tsx
+++ components/About/Skill.tsx
@@ -1,6 +1,7 @@
 import styles from '../../styles/About.module.scss';
 import React from 'react';
 import SkillSet from './SkillSet';
+import classNames from 'classnames';
 
 type SkillProps = {
     programmingSkill: string[],
@@ -12,18 +13,9 @@ const Skill: React.FC<SkillProps> = ({programmingSkill, framework, knowledge}) =
 
     return (
     <section className={styles.skill}>
-        <div className={styles.skillCategory}>
-            <h2 className={styles.skillCategoryTitle}>Programming Skills</h2>
-            <SkillSet skills={programmingSkill} />
-        </div>
-        <div className={styles.skillCategory}>
-            <h2 className={styles.skillCategoryTitle}>Software / Framework</h2>
-            <SkillSet skills={framework} />
-        </div>
-        <div className={styles.skillCategory}>
-            <h2 className={styles.skillCategoryTitle}>Collaboration / Knowledge</h2>
-            <SkillSet skills={knowledge} />
-        </div>
+        <SkillSet title={"Programming Skills"} skills={programmingSkill} />
+        <SkillSet title="Software / Framework" skills={framework} />
+        <SkillSet title="Collaboration / Knowledge" skills={knowledge} />
     </section>
     );
 };
--- components/About/SkillSet.tsx
+++ components/About/SkillSet.tsx
@@ -1,19 +1,26 @@
 import styles from '../../styles/About.module.scss';
 import React from 'react';
+import classNames from 'classnames';
 
 type Props = {
+    title: string
     skills: string[]
 }
 
-const SkillSet: React.FC<Props> = ({skills}) => {
+const SkillSet: React.FC<Props> = ({title, skills}) => {
 
     return (
-    <div className={styles.skillCategoryContent}>
-        {skills.map((skill) => (
-            <div key={skill} className={styles.skillCategoryItem}>
-                <div className={styles.skillCategoryItemTitle}>{skill}</div>
-            </div>
-        ))}
+    <div className={styles.skillCategory}>
+        <h2 className={styles.skillCategoryTitle}>
+            <span className={classNames('l', 'colorBlack')}>{title}</span>
+        </h2>
+        <div className={styles.skillCategoryContent}>
+            {skills.map((skill) => (
+                <div key={skill} className={styles.skillCategoryContentItem}>
+                    <span className={classNames('m', 'colorWhite')}>{skill}</span>
+                </div>
+            ))}
+        </div>
     </div>
     );
 };
