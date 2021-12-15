716df53dc9aa2b944f7b2fdcef7ac52f2ad4f3ea
Willake <willakelin@gmail.com>
2021-12-15 11:10:08 +0800

    update contact

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,6 +1,7 @@
 import styles from '../styles/Contact.module.scss';
 import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
 import React from 'react';
+import classNames from 'classnames';
 
 const Contact: React.FC = () => {
 
@@ -8,19 +9,19 @@ const Contact: React.FC = () => {
         <section id="contact" className={styles.contact}>
             <div className={styles.contactButton}>
                 <a title="Email Contact" className={styles.contactButtonLink} href="mailto:willakelin@gmail.com">
-                    Contact me
+                    <span className={classNames('l', 'colorWhite')}>Contact me</span>
                 </a>
             </div>
             <div className={styles.contactEmail}>willakelin@gmail.com</div>
             <div className={styles.contactLinks}>
                 <a title="LinkedIn Contact" className={styles.contactLink} href="https://www.linkedin.com/in/willakelin/">
-                    <FaLinkedin />
+                    <FaLinkedin size={32}/>
                 </a>
                 <a title="Github Contact" className={styles.contactLink} href="https://github.com/willake">
-                    <FaGithub />
+                    <FaGithub size={32}/>
                 </a>
                 <a title="Twitter Contact" className={styles.contactLink} href="https://twitter.com/WillakeLin">
-                    <FaTwitter />
+                    <FaTwitter size={32}/>
                 </a>
             </div>
         </section>
