f2f1f4fecafdecfc232088e7660a7e53cd136d4f
Willake <willakelin@gmail.com>
2021-12-15 11:28:34 +0800

    update intro

--- components/About/Intro.tsx
+++ components/About/Intro.tsx
@@ -1,5 +1,7 @@
 import styles from '../../styles/About.module.scss';
 import React from 'react';
+import Image from 'next/image';
+import classNames from 'classnames';
 
 type IntroProps = {
     intro: string[]
@@ -8,12 +10,17 @@ type IntroProps = {
 const Intro: React.FC<IntroProps> = ({intro}) => {
 
     return (
-        <section className={styles.aboutMe}>
-            <div className={styles.aboutMeAvatar}>
+        <section id="intro" className={styles.intro}>
+            <div>
+                <div className={styles.introAvatar}>
+                    <Image src='/images/about/me.jpg' alt="me" width="150" height="150" layout="fill" objectFit="contain"/>
+                </div>
             </div>
-            <div className={styles.aboutMeContent}>
+            <div className={styles.introTexts}>
                 {intro.map((text) => (
-                    <div key={text.length} className={styles.aboutMeContentItem}>{text}</div>
+                    <div key={text.length} className={styles.introText}>
+                        <span className={classNames('m', 'colorBlack')}>{text}</span>
+                    </div>
                 ))}
             </div>
         </section>
