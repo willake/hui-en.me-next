9d69f9135589f98d95b2b3e21510d198f7b83e1e
Willake <willakelin@gmail.com>
2021-12-15 12:55:52 +0800

    update home header

--- components/Home/Header.tsx
+++ components/Home/Header.tsx
@@ -1,20 +1,37 @@
 import styles from '../../styles/Home.module.scss';
-import React from 'react';
+import React, { useRef } from 'react';
+import classNames from 'classnames';
+import { isMobile } from 'react-device-detect';
 
 const Header: React.FC = () => {
+    const videoRef = useRef<HTMLVideoElement>(null);
+
+    const handlePlayVideo = () => {
+        if(videoRef.current !== null) {
+            videoRef.current.play();
+        }
+    }
+
+    setTimeout(() => {
+        if(!isMobile) handlePlayVideo();
+    }, 200);
 
     return (
         <header className={styles.header}>
             <div className={styles.headerContent}>
-                {/* <video id="intro__video" class="intro__video" loop="loop" muted="muted" preload="preload">
-                        <source src="{{ $mp4.Permalink }}" type="video/mp4">
-                        <source src="{{ $webm.Permalink }}" type="video/webm">
-                </video> */}
+                <video id="headerVideo" ref={videoRef} className={styles.headerVideo} loop={true} muted={true} preload="preload">
+                        <source src="/videos/portfolio.mp4" type="video/mp4" />
+                        <source src="/videos/portfolio.webm" type="video/webm" />
+                </video>
                 <div className={styles.headerOverlay}>
                 </div>
                 <div className={styles.headerTexts}>
-                    <h1 className={styles.headerTitle}>- Hui En Lin -</h1>
-                    <h2 className={styles.headerSubtitle}>Gameplay Programmer</h2>
+                    <h1 className={styles.headerTitle}>
+                        <span className={classNames('xxl', 'colorWhite')}>- Hui En Lin -</span>
+                    </h1>
+                    <h2 className={styles.headerSubtitle}>
+                        <span className={classNames('xl', 'colorWhite')}>Gameplay Programmer</span>
+                    </h2>
                 </div>
             </div>
         </header>
