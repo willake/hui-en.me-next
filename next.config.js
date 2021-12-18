6345935198fa94311b5b7f600da89a74634b2d3b
Willake <willakelin@gmail.com>
2021-12-18 21:10:47 +0800

    remove useless packages

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  webpack: (config, options) => {
-    config.module.rules.push({
-      test: /\.md$/,
-      use: "raw-loader"
-    },)
-
-    return config
-  }
   // exportPathMap: async function (
   //   defaultPathMap,
   //   { dev, dir, outDir, distDir, buildId }
