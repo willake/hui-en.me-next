99a7a07c3c7fe192b31cef8d45cc4f674b2af6ff
Willake <willakelin@gmail.com>
2021-12-19 20:46:39 +0800

    update close image

--- components/Nav/Menu.tsx
+++ components/Nav/Menu.tsx
@@ -2,7 +2,7 @@ import styles from '../../styles/Nav.module.scss';
 import React from 'react';
 import Link from 'next/link';
 import classNames from 'classnames';
-import { FaTimes } from "react-icons/fa";
+import { AiOutlineClose } from "react-icons/ai";
 
 type Props = {
     isOpen: boolean,
@@ -16,7 +16,7 @@ const Menu: React.FC<Props> = ({isOpen, closeMenu}) => {
         <div className={styles.menu}>
             <div className={styles.menuHeader}>
                 <div onClick={closeMenu} className={classNames(styles.closeButton, 'colorWhite')}>
-                    <FaTimes size={46}/>
+                    <AiOutlineClose size={46}/>
                 </div>
             </div>
             <ul>
