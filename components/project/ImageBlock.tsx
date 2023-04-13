03b2701178d036ae33dc17e8c9e1d2b16d05584b
Lincoln <willakelin@seekrtech.com>
2023-04-13 18:28:29 +0200

    update: add project pages

--- components/project/ImageBlock.tsx
+++ components/project/ImageBlock.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { CSSProperties } from "react";
 import styles from "../../styles/project.module.scss";
 import Image from "next/image";
 
@@ -10,9 +10,10 @@ type Props = {
 }
 
 const ImageBlock: React.FC<Props> = ({ alt, src, width, height }) => {
+
     return (
         <div className={styles.contentImage}>
-            <Image alt={alt} src={src} width={width} height={height} layout="responsive"/>
+            <Image alt={alt} src={src} width={width} height={height} layout="intrinsic"/>
         </div>
     );
 };
