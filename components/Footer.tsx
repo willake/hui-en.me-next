6a3b110ab551bac6fd38fba499e9517bc3513794
willake <willakelin@gmail.com>
2023-04-13 22:06:08 +0200

    update: update style of Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,17 +1,31 @@
 import styles from '../styles/Footer.module.scss';
 import React from 'react';
 import classNames from 'classnames';
+import { styled } from 'styles';
+import { H2, VFlex } from 'styles/Common';
+import { DARK_GREEN } from 'styles/color';
 
 const Footer: React.FC = () => {
   const year = new Date().getFullYear();
 
   return (
-    <footer className={styles.footer}>
-      <h2 className={classNames(styles.footerCopyright, 'm', 'colorWhite')}>
+    <FooterBox>
+      <H2
+        css={{ fontWeight: 500, margin: '20px' }}
+        size={'m'}
+        textColor={'white'}
+      >
         Copyright © Hui En Lin, 2016 - {year}
-      </h2>
-    </footer>
+      </H2>
+    </FooterBox>
   );
 };
 
 export default Footer;
+
+const FooterBox = styled(VFlex, {
+  backgroundColor: DARK_GREEN,
+  justifyContent: 'center',
+  alignItems: 'center',
+  textAlign: 'center',
+});
